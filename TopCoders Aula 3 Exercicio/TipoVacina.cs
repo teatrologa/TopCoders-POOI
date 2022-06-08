588df7eb927020c164678788137f7582453cb29d
teatrologa <li.castrinho@gmail.com>
2022-06-08 17:54:44 -0300

    Vacina + TipoVacina

---
+++ TopCoders Aula 3 Exercicio/TipoVacina.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopCoders_POOI_Aula3_Exercicio.Classes;
+
+namespace TopCoders_Aula_3_Exercicio
+{
+    public enum TipoVacina
+    {
+        Raiva,
+        V10,
+        V4
+    }
+}
+
+
+
+/*
+ Nome da vacina: Covid
+ Tipo: Coronavac, Astrazenica, etc.
+
+    Ou o contrário:
+    Nome: Coronavac, Astrazenica etc.
+    Tipo: Covid
+
+    IDEIA: DICIONÁRIO COM CHAVE: ESPECIE
+           Tipo de Vac, Nome
+    >> Ele precisa ser aberto a adição de componentes
+
+    
+ */
---
+++ TopCoders Aula 3 Exercicio/Vacina.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopCoders_POOI_Aula3_Exercicio.Enums;
+
+namespace TopCoders_Aula_3_Exercicio.Classes
+{
+    public class TipoVacina
+    {
+        public Guid IDuniVacina { get; } = Guid.NewGuid();
+        public string nome = "";
+        public string lote = "";
+        public DateTime dataAplicacao;
+        public int duracaoVacina;
+        public TipoVacina tipoVacina;
+        
+        
+        public List<Especie> especie;
+
+        public Guid ObterIdUnicoVacina()
+        {
+            return IDuniVacina;
+        }
+        
+            
+        
+
+    }
+}
+
+/*
+Pet - ID
+Cart.Vac - ID
+
+tipo de vacina: covid
+nome de vacina: coronavac, astrazenica, etc. - ID unico 
+
+
+*/
