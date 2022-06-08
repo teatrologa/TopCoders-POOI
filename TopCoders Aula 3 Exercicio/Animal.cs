a667b3a3c0f8cc0326044c09b607cf08dd1c876b
teatrologa <li.castrinho@gmail.com>
2022-06-08 12:17:20 -0300

    Exercicio Aula 3

---
+++ TopCoders Aula 3 Exercicio/Animal.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopCoders_POOI_Aula3_Exercicio.Enums;
+
+namespace TopCoders_POOI_Aula3_Exercicio.Classes
+
+{
+    public class Animal
+    {
+        public Guid codigo { get; } = Guid.NewGuid();
+        public string nome = "";
+        public string raca = "";
+        public string cor = "";
+        public decimal peso;
+        public int idade;
+        public DateTime nascimento;
+        public bool agressividade;
+        public char sexo;
+        public bool castracao;
+        public DateTime dataCadastro { get; } = DateTime.Now;
+        public List<string> doencasAlergias { get; set; } = new List<string>();
+
+        public Especie especie;
+        public Porte porte;
+
+        // COMENTADO POIS JÁ ESTÁ COM O GET ALI EM CIMA
+        //public Guid ObterCodigo()
+        //{
+        //    return codigo;
+        //}
+
+        public DateTime ObterDataCadastro()
+        {
+            return dataCadastro;
+        }
+
+        public void RegistrarNascimento(int ano, int mes, int dia = 1)
+        {
+            nascimento = new DateTime(ano, mes, dia);
+        }
+
+        public int Idade()
+        {
+            idade = (DateTime.Now.Year - nascimento.Year);
+            return idade;
+        }
+
+        public bool NecessidadesEspeciais()
+        {
+            return doencasAlergias.Any();
+        }
+
+        public List<string> ObterNecessidadesEspecieais()
+        {
+            return doencasAlergias;
+        }
+
+        public void AdicionarDoencasAlergias(string doencasAlergias)
+        {
+            this.doencasAlergias.Add(doencasAlergias);
+        }
+
+
+
+        public void ImprimirAnimal()
+        {
+            Console.WriteLine("\t Ficha do pet.");
+            Console.WriteLine("\t _____________");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Codigo: {codigo}"); // Forma 1 de escrever
+            Console.Write("Nome: ");  // Forma 2 de escrever Write + WriteLine
+            Console.WriteLine(nome);
+            Console.WriteLine($"Idade: {Idade()} anos");
+            Console.WriteLine($"Espécie: {especie}");
+            Console.WriteLine($"Raça: {raca}");
+            Console.WriteLine($"Cor: {cor}");
+            Console.WriteLine($"Porte: { porte}");
+            Console.WriteLine($"Peso: {peso}");
+            Console.WriteLine($"Nascimento: {nascimento}");
+            Console.WriteLine($"É agressivo? {agressividade}");
+            Console.WriteLine($"Realizou castração? {castracao}");
+            Console.WriteLine(" ");
+
+
+            if (NecessidadesEspeciais())
+            {
+                Console.WriteLine("Doenças e Alergias:");
+                foreach (string doencaalergia in doencasAlergias)
+                {
+                    Console.WriteLine(doencaalergia);
+                }
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"Pet cadastrado em: {dataCadastro}");
+            Console.WriteLine(" ");
+
+        }
+    }
+}
---
+++ TopCoders Aula 3 Exercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿using TopCoders_POOI_Aula3_Exercicio.Enums;
+using TopCoders_POOI_Aula3_Exercicio.Classes;
+
+namespace TopCoders_POOI_Aula3_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.nome = "Ratita";
+            animal.raca = "Yorkshire";
+            animal.cor = "Padrão da raça";
+            animal.peso = 2.5m;
+            animal.RegistrarNascimento(2016, 08);
+            animal.agressividade = false;
+            animal.sexo = 'F';
+            animal.castracao = true;
+            Guid codigoAnimal = animal.codigo;
+            animal.especie = Especie.Cachorro;
+            animal.porte = Porte.Pequeno;
+            animal.AdicionarDoencasAlergias("Alergia: Nozes");
+            animal.AdicionarDoencasAlergias("Alergia: Cheiro forte");
+
+            animal.ImprimirAnimal();
+        }
+    }
+
+}
